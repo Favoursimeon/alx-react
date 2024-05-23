6430f29c15913a9a1d3ddf141b2970e58136e895
Favoursimeon <favoursimeon12w@gmail.com>
2024-05-23 18:30:48 +0000

    React

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,5 +1,5 @@
-import $ from "jquery";
-import _ from 'lodash';
+const $ = require('jquery');
+const _ = require('lodash');
 
 $('body').append('<p>Holberton Dashboard</p>');
 $('body').append('<p>Dashboard data for the students</p>');
@@ -7,12 +7,13 @@ $('body').append('<button>Click here to get started</button>');
 $('body').append("<p id='count'></p>");
 $('body').append('<p>Copyright - Holberton School</p>');
 
+const updateCounter = () => {
+	let times = $('#count').html() || 0;
+	$('button').on('click', () => {
+		times++;
+		$('#count').html(`${times} clicks on the button`);
+	});
+};
 
-let count = 0;
-
-function updateCounter() {
-	  count += 1;
-	  $('#count').text(`${count} clicks on the button`);
-}
-
-$('button').on('click', _.debounce(updateCounter, 500));
+_.debounce(updateCounter, 500);
+updateCounter();
